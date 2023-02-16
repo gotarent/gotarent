f068a7d758ae514508800e6b0f9c0355fac2ac1d
abhisekadhikari <89593428+abhisekadhikari@users.noreply.github.com>
2023-02-16 15:48:24 +0530

    Adding Client

--- Server/Routes/route.js
+++ Server/Routes/route.js
@@ -21,15 +21,17 @@ route.post(
     check("phone", "Enter valid number")
       .isNumeric()
       .exists()
-      .isLength({ max: 10 }),
-    check("password", "Enter valid password").exists().isLength({ min: 8 }),
+      .isLength({ min: 10 }),
+    check("password", "Password connot be lesser than 8 characters")
+      .exists()
+      .isLength({ min: 8 }),
   ],
   async (req, res) => {
     const { name, email, phone, password } = req.body
     const error = validationResult(req)
     if (!error.isEmpty()) {
       const alert = error.array()
-      return res.status(404).json({ alert })
+      return res.status(404).json({ message: alert })
     }
     const userEmail = await User.findOne({ email: email })
     if (userEmail) {
@@ -44,12 +46,10 @@ route.post(
       return res.status(200).json({ message: "Signup UnSuccessful" })
     }
     await sendMail(email, result._id.toString())
-    res
-      .status(200)
-      .json({
-        message: "Signup Successful and Email Send Successfully",
-        userId: result._id,
-      })
+    res.status(200).json({
+      message: "Signup Successful and Email Send Successfully",
+      userId: result._id,
+    })
   }
 )
 
